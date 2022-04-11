005619f86326cf1c2d1f331ac0671513d5c7ff7d
Rinnegatamante <rinnegatamante@gmail.com>
2022-04-12 00:49:54 +0200

    Added custom bubbles builder application.

---
+++ launcher/main.c
@@ -0,0 +1,23 @@
+#include <vitasdk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char *argv[]) {
+	char buffer[256];
+	memset(buffer, 0, 256);
+	FILE *f = fopen("app0:args.txt", "rb");
+	if (f) {
+		fread(buffer, 1, 256, f);
+		fclose(f);
+		char uri[512];
+		sprintf(uri, "psgm:play?titleid=%s&param=%s", "YYOLOADER", buffer);
+
+		sceAppMgrLaunchAppByUri(0xFFFFF, uri);
+		sceKernelDelayThread(1000);
+	}
+	
+	sceKernelExitProcess(0);
+  
+	return 0;
+}
