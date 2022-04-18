fa53d832e20f36b9827f79f64d6010e64ea1bd7a
Rinnegatamante <rinnegatamante@gmail.com>
2022-04-18 10:57:12 +0200

    Minor adjustments to standalone bubbles creator script.

--- launcher/builder/build.bat
+++ launcher/builder/build.bat
@@ -1,5 +1,4 @@
--@set /p title="Insert bubble name: "
-@set /p rom="Insert game name (folder): "
+@set /p title="Insert bubble name: "
 @set /p id="Insert bubble title ID (9 characters [NOTE: Only UPPERCASE letters or numbers]): "
 vita-mksfoex -s TITLE_ID=%id% "%title%" param.sfo
 vita-pack-vpk -s param.sfo -b eboot.bin "%title%.vpk" -a game.apk=game.apk -a assets/icon0.png=sce_sys/icon0.png -a assets/bg.png=sce_sys/livearea/contents/bg.png -a assets/startup.png=sce_sys/livearea/contents/startup.png -a assets/template.xml=sce_sys/livearea/contents/template.xml -a assets/args.txt=args.txt
