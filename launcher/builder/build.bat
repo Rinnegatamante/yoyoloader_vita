5bc76a71489e721e7f6d29304f7e927d143ada4f
Rinnegatamante <rinnegatamante@gmail.com>
2022-04-19 18:51:13 +0200

    Properly setting ATTRIBUTE2 flag on standalone builds.

--- launcher/builder/build.bat
+++ launcher/builder/build.bat
@@ -1,4 +1,4 @@
 @set /p title="Insert bubble name: "
 @set /p id="Insert bubble title ID (9 characters [NOTE: Only UPPERCASE letters or numbers]): "
-vita-mksfoex -s TITLE_ID=%id% "%title%" param.sfo
-vita-pack-vpk -s param.sfo -b eboot.bin "%title%.vpk" -a game.apk=game.apk -a assets/icon0.png=sce_sys/icon0.png -a assets/bg.png=sce_sys/livearea/contents/bg.png -a assets/startup.png=sce_sys/livearea/contents/startup.png -a assets/template.xml=sce_sys/livearea/contents/template.xml -a assets/args.txt=args.txt
+vita-mksfoex -d ATTRIBUTE2=12 -s TITLE_ID=%id% "%title%" param.sfo
+vita-pack-vpk -s param.sfo -b eboot.bin "%title%.vpk" -a game.apk=game.apk -a assets/icon0.png=sce_sys/icon0.png -a assets/bg.png=sce_sys/livearea/contents/bg.png -a assets/startup.png=sce_sys/livearea/contents/startup.png -a assets/template.xml=sce_sys/livearea/contents/template.xml
